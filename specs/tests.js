c68a3176247f485d41f8de0e0dd8aa21b5c76c7a
Wojcikowski Frederic <wojcikowskif@gmail.com>
2020-12-11 12:26:38 +0900

    First commit

---
+++ specs/tests.js
@@ -0,0 +1,93 @@
+describe("gameGenerator()", () => {
+  let game, limit;
+
+  beforeEach(() => {
+    game = gameGenerator(30);
+    limit = 30;
+  });
+
+  it("should be there", () => {
+    expect(gameGenerator).toBeDefined();
+    expect(typeof gameGenerator).toBe("function");
+  });
+
+  it("should generate some games", () => {
+    expect(typeof game).toBe("object");
+  });
+
+  it("game should have a guess method", () => {
+    expect(game.guess).toBeDefined();
+    expect(typeof game.guess).toBe("function");
+  });
+
+  it("game should have a numberGuesses method", () => {
+    expect(game.numberGuesses).toBeDefined();
+    expect(typeof game.numberGuesses).toBe("function");
+  });
+
+  it("game should have a reset method", () => {
+    expect(game.reset).toBeDefined();
+    expect(typeof game.reset).toBe("function");
+  });
+
+  it("game should have a giveUp method", () => {
+    expect(game.giveUp).toBeDefined();
+    expect(typeof game.giveUp).toBe("function");
+  });
+
+  describe("game.guess()", () => {
+    it("should have just one winning number", () => {
+      const number = [];
+      for (let i = 0; i <= limit; i++) {
+        if (game.guess(i)) number.push(i);
+      }
+      expect(number.length).toBe(1);
+    });
+  });
+
+  describe("game.numberGuesses()", () => {
+    it("should return the correct number of guesses", () => {
+      game.guess(2);
+      game.guess(1);
+      expect(game.numberGuesses()).toBe(2);
+    });
+  });
+
+  describe("game.reset()", () => {
+    it("should reset the guesses", () => {
+      game.guess(2);
+      game.guess(1);
+      game.reset();
+      expect(game.numberGuesses()).toBe(0);
+    });
+
+    it("should generate a new number to guess", () => {
+      let correctNumber, newNumber;
+      for (let i = 0; i <= limit; i++) {
+        if (game.guess(i)) correctNumber = i;
+      }
+      const oldNumber = correctNumber;
+      for (let i = 0; i <= limit; i++) {
+        if (game.guess(i)) newNumber = i;
+      }
+      console.log(oldNumber, newNumber);
+      expect(oldNumber !== newNumber).toBeFalsy();
+    });
+  });
+
+  describe("game.giveUp()", () => {
+    it("should return the correct number", () => {
+      let correctNumber;
+      for (let i = 0; i <= limit; i++) {
+        if (game.guess(i)) correctNumber = i;
+      }
+      expect(game.giveUp()).toBe(correctNumber);
+    });
+
+    it("should use the game.reset() method to reset the game", () => {
+      const spy = spyOn(game, "reset");
+      game.giveUp();
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+});
---
+++ src/closures.js
@@ -0,0 +1,35 @@
+/* exported gameGenerator accountGenerator randomInteger */
+
+function randomInteger(limit) {
+  return Math.floor(Math.random() * (limit + 1));
+}
+
+function gameGenerator(limit) {
+  const numberToGuess = randomInteger(limit);
+  let guesses = 0;
+
+  return {
+    guess(guessNumber) {
+      guesses++;
+      return guessNumber === numberToGuess ? true : false;
+    },
+
+    reset() {
+      guesses = 0;
+      const oldNumber = numberToGuess;
+      while (oldNumber !== numberToGuess) {
+        numberToGuess = randomInteger(limit);
+      }
+    },
+
+    giveUp() {
+      const result = numberToGuess;
+      this.reset();
+      return result;
+    },
+
+    numberGuesses() {
+      return guesses;
+    },
+  };
+}
